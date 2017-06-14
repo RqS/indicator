b508726b7f58092d750c9f16388e7dc2660b5ef1
Runqi Shao <shaorunqi@gmail.com>
2017-06-14 15:32:23 -0700

    ready for classify

---
+++ ft_classifier.py
@@ -0,0 +1,14 @@
+import sys
+import random
+import fasttext
+
+data = sys.argv[1]
+training_data = sys.argv[2]
+#training_data = '/Users/runqishao/Desktop/dig_indicator/training_data.txt'
+#data = '/Users/runqishao/Desktop/dig_indicator/dataset.txt'
+
+data_f = open(data, 'r')
+train_f = open(training_data, 'w')
+
+test_text, test_label = generate_train_and_test(data_f, train_f)
+
