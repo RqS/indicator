1a070f8a317e7938bf7fe0c6c903787f5b939902
Jiayuan Ding <jiayuanding@guest-174.isi.edu>
2017-06-15 13:59:56 -0700

    modified:ft_classifier.py

--- ft_classifier.py
+++ ft_classifier.py
@@ -4,17 +4,16 @@ import fasttext
 import process_functions as pf
 import json
 import codecs
-
+import numpy
 
 data = sys.argv[1]
 training_data = sys.argv[2]
 result_data = sys.argv[3]
-#training_data = '/Users/runqishao/Desktop/dig_indicator/training_data.txt'
-#data = '/Users/runqishao/Desktop/dig_indicator/dataset.txt'
 
-data_f = open(data, 'r')
-train_f = open(training_data, 'w')
-result_f = codecs.open(result_data, 'w', 'utf-8')
+
+data_f = open(data, 'r')   #dataset containing training and test datasets
+train_f = open(training_data, 'w')  # save as trainging datasets
+result_f = codecs.open(result_data, 'w', 'utf-8')  #save as test prediction result
 
 test_text, test_label = pf.generate_train_and_test(data_f, train_f)
 
@@ -23,48 +22,49 @@ train_f.close()
 
 #for word_ngrams in [1]:
 num=0
-for epoch in range(4, 21, 2):
-	for neg in range(4, 17, 2):
-		for lr in [x / 100.0 for x in range(5, 51, 5)]:
+for epoch in range(4, 21, 1):
+	for dim in range(60, 220, 20):
+		for lr in numpy.arange(0.01, 0.26, 0.01):
 			num = num + 1
 			print ("Tune parameter no.%d" %num)
-			model_name = "movement_model/movement_classifier"
-			model_name = model_name+"_lr_"+str(lr)+"_neg_"+str(neg)+"_epoch_"+str(epoch)
-			classifier = fasttext.supervised(training_data, model_name,
-						dim=200, lr=lr, epoch=epoch, neg=neg)
+			classifier = fasttext.supervised(training_data, "movement_model",
+						lr=lr, dim=dim, epoch=epoch)
 			predict_labels = classifier.predict(test_text)
 
-			tp=0
-			fp = 0
-			tn = 0
-			fn = 0
+			t_t = 0
+                        t_f = 0
+                        f_t = 0
+                        f_f = 0
+                        
 			for i in range(len(predict_labels)):
-			    if predict_labels[i][0] == "TRUE" and test_label[i] == "FALSE":
-			        fp = fp + 1
-			    elif test_label[i] == "TRUE" and predict_labels[i][0] == "FALSE":
-			        fn = fn + 1
-			    elif predict_labels[i][0] == "TRUE" and test_label[i] == "TRUE":
-			        tp = tp + 1
-			    else:
-			    	tn = tn + 1
+				if test_label[i] == 'TRUE' and predict_labels[i][0].encode('utf-8') == 'TRUE':
+                                        t_t += 1
+                                elif test_label[i] == 'TRUE' and predict_labels[i][0].encode('utf-8') == 'FALSE':
+                                        t_f += 1
+                                elif test_label[i] == 'FALSE' and predict_labels[i][0].encode('utf-8') == 'TRUE':
+                                        f_t += 1
+                                elif test_label[i] == 'FALSE' and predict_labels[i][0].encode('utf-8') == 'FALSE':
+                                        f_f += 1 
 
 			this_result = {
 				"prams": {
 					"lr": lr,
-					"neg": neg,
-					"epoch": epoch
+					"dim": dim,
+					"epoch": epoch,
+                                        
 				},
 				"stats": {
-					"tp": tp,
-					"fp": fp,
-					"fn": fn,
-					"tn": tn,
-					"precision": tp/float(fp+tp),
-					"recall": tp/float(tp+fn)
+					"t_t": t_t,
+					"t_f": t_f,
+					"f_t": f_t,
+					"f_f": f_f,
+					"precision": t_t / float(t_t + f_t),
+					"recall": t_t / float(t_t + t_f)
 				}
 			}
 
 			json.dump(this_result, result_f)
 			result_f.write("\n")
 
+result_f.close()
 
