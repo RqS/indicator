fafb769f45722bf8a9bde1c2470be69cf08ca401
Runqi Shao <shaorunqi@gmail.com>
2017-06-15 20:22:38 -0700

    modified for incall and outcall label

--- outcall_label.py
+++ outcall_label.py
@@ -3,22 +3,14 @@ import codecs
 import json
 import process_functions as pf
 import spacy
-import movement_extractor
-#import incall_extractor
-#import fasttext
-#import random
+import outcall_extractor
 
-#input_file = '/Users/runqishao/Downloads/eccie_1000_content_result.jl'
-#text_part = 'content_relaxed'
-#output_file = '/Users/runqishao/Downloads/label_result.jl'
 input_file = sys.argv[1]
 positive_text = sys.argv[2]
 negative_text = sys.argv[3]
 
-
 nlp = spacy.load('en')
-movement_matcher = movement_extractor.load_movement_matcher(nlp)
-#incall_matcher = incall_extractor.load_movement_matcher(nlp)
+outcall_matcher = outcall_extractor.load_outcall_matcher(nlp)
 nlp = pf.prep_nlp(nlp)
 
 positive_f = open(positive_text, 'w')
@@ -30,9 +22,8 @@ with open(input_file, 'r') as f:
             print "process line no.%d" %index
         t = pf.extract_crftokens(sentence.decode("utf-8"), lowercase=False)
         t_simple_tokens = pf.extract_tokens_from_crf(t)
-        movement = movement_extractor.extract(nlp(t_simple_tokens), movement_matcher)
-        #incall = incall_extractor.extract(nlp(t_simple_tokens), incall_matcher)
-        this_label = pf.process_extracted(movement)
+        outcall = outcall_extractor.extract(nlp(t_simple_tokens), outcall_matcher)
+        this_label = pf.process_extracted(outcall)
         if this_label == "TRUE":
             positive_f.write("__label__%s %s" % (this_label, sentence))
         else:
