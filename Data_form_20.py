b67a3df9e847981b12744f5d406815d5da6c35a9
Jiayuan Ding <jiayuanding@guest-126.isi.edu>
2017-06-19 13:25:33 -0700

    modified Data_form_20.py

--- Data_form_20.py
+++ Data_form_20.py
@@ -12,7 +12,7 @@ for i in range(200):
     neg_lines = open('/home/ubuntu/dig_indicator/data/incall_data/incall_neg.txt').read().splitlines()
     myline =random.choice(neg_lines)
     if myline not in lst:
-        total_data_f.write("%d.%s\n" %(i,myline))
+        total_data_f.write("%d.%s\n" %(k,myline))
         k += 1
         lst.append(myline)
         if k == 20:
@@ -24,7 +24,7 @@ for i in range(200):
     neg_lines = open('/home/ubuntu/dig_indicator/data/incall_data/incall_sn_p.txt').read().splitlines()
     myline =random.choice(neg_lines)
     if myline not in lst:
-        total_data_f.write("%d.%s\n" %(i,myline))
+        total_data_f.write("%d.%s\n" %(k,myline))
         k += 1
         lst.append(myline)
         if k == 20:
@@ -36,7 +36,7 @@ for i in range(200):
     neg_lines = open('/home/ubuntu/dig_indicator/data/incall_data/incall_pos.txt').read().splitlines()
     myline =random.choice(neg_lines)
     if myline not in lst:
-        total_data_f.write("%d.%s\n" %(i,myline))
+        total_data_f.write("%d.%s\n" %(k,myline))
         k += 1
         lst.append(myline)
         if k == 20:
@@ -50,7 +50,7 @@ for i in range(200):
     neg_lines = open('/home/ubuntu/dig_indicator/data/incall_data/incall_sp_n.txt').read().splitlines()
     myline =random.choice(neg_lines)
     if myline not in lst:
-        total_data_f.write("%d.%s\n" %(i,myline))
+        total_data_f.write("%d.%s\n" %(k,myline))
         k += 1
         lst.append(myline)
         if k == 20:
@@ -62,7 +62,7 @@ for i in range(200):
     neg_lines = open('/home/ubuntu/dig_indicator/data/incall_data/incall_p_n.txt').read().splitlines()
     myline =random.choice(neg_lines)
     if myline not in lst:
-        total_data_f.write("%d.%s\n" %(i,myline))
+        total_data_f.write("%d.%s\n" %(k,myline))
         k += 1
         lst.append(myline)
         if k == 20:
@@ -75,7 +75,7 @@ for i in range(200):
     neg_lines = open('/home/ubuntu/dig_indicator/data/incall_data/incall_sp_sn.txt').read().splitlines()
     myline =random.choice(neg_lines)
     if myline not in lst:
-        total_data_f.write("%d.%s\n" %(i,myline))
+        total_data_f.write("%d.%s\n" %(k,myline))
         k += 1
         lst.append(myline)
         if k == 20:
@@ -88,7 +88,7 @@ for i in range(200):
     neg_lines = open('/home/ubuntu/dig_indicator/data/incall_data/incall_ne.txt').read().splitlines()
     myline =random.choice(neg_lines)
     if myline not in lst:
-        total_data_f.write("%d.%s\n" %(i,myline))
+        total_data_f.write("%d.%s\n" %(k,myline))
         k += 1
         lst.append(myline)
         if k == 20:
