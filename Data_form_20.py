8324dec836dd13d5245154cd6901166f847468b9
Jiayuan Ding <jiayuanding@guest-126.isi.edu>
2017-06-19 13:16:27 -0700

    modified Data_form_20.py

--- Data_form_20.py
+++ Data_form_20.py
@@ -6,42 +6,93 @@ import random
 
 total_data_f = open("dataset_20.txt", 'w')
 
-
-for i in range(20):
+lst = []
+k = 0
+for i in range(200):
     neg_lines = open('/home/ubuntu/dig_indicator/data/incall_data/incall_neg.txt').read().splitlines()
     myline =random.choice(neg_lines)
-    total_data_f.write("%d.%s\n" %(i,myline))
+    if line not in lst:
+        total_data_f.write("%d.%s\n" %(i,myline))
+        k += 1
+        lst_1.append(myline)
+        if k == 20:
+            break
 
-
-for i in range(20):
+lst = []
+k = 0
+for i in range(200):
     neg_lines = open('/home/ubuntu/dig_indicator/data/incall_data/incall_sn_p.txt').read().splitlines()
     myline =random.choice(neg_lines)
-    total_data_f.write("%d.%s\n" %(i,myline))
-    
-for i in range(20):
+    if line not in lst:
+        total_data_f.write("%d.%s\n" %(i,myline))
+        k += 1
+        lst_1.append(myline)
+        if k == 20:
+            break
+
+lst = []
+k = 0
+for i in range(200):
     neg_lines = open('/home/ubuntu/dig_indicator/data/incall_data/incall_pos.txt').read().splitlines()
     myline =random.choice(neg_lines)
-    total_data_f.write("%d.%s\n" %(i,myline))
+    if line not in lst:
+        total_data_f.write("%d.%s\n" %(i,myline))
+        k += 1
+        lst_1.append(myline)
+        if k == 20:
+            break
 
-for i in range(20):
+
+
+lst = []
+k = 0
+for i in range(200):
     neg_lines = open('/home/ubuntu/dig_indicator/data/incall_data/incall_sp_n.txt').read().splitlines()
     myline =random.choice(neg_lines)
-    total_data_f.write("%d.%s\n" %(i,myline))
+    if line not in lst:
+        total_data_f.write("%d.%s\n" %(i,myline))
+        k += 1
+        lst_1.append(myline)
+        if k == 20:
+            break
 
-for i in range(20):
+lst = []
+k = 0
+for i in range(200):
     neg_lines = open('/home/ubuntu/dig_indicator/data/incall_data/incall_p_n.txt').read().splitlines()
     myline =random.choice(neg_lines)
-    total_data_f.write("%d.%s\n" %(i,myline))
+    if line not in lst:
+        total_data_f.write("%d.%s\n" %(i,myline))
+        k += 1
+        lst_1.append(myline)
+        if k == 20:
+            break
 
-for i in range(20):
+    
+lst = []
+k = 0
+for i in range(200):
     neg_lines = open('/home/ubuntu/dig_indicator/data/incall_data/incall_sp_sn.txt').read().splitlines()
     myline =random.choice(neg_lines)
-    total_data_f.write("%d.%s\n" %(i,myline))
+    if line not in lst:
+        total_data_f.write("%d.%s\n" %(i,myline))
+        k += 1
+        lst_1.append(myline)
+        if k == 20:
+            break
 
-for i in range(20):
+
+lst = []
+k = 0
+for i in range(200):
     neg_lines = open('/home/ubuntu/dig_indicator/data/incall_data/incall_ne.txt').read().splitlines()
     myline =random.choice(neg_lines)
-    total_data_f.write("%d.%s\n" %(i,myline))
+    if line not in lst:
+        total_data_f.write("%d.%s\n" %(i,myline))
+        k += 1
+        lst_1.append(myline)
+        if k == 20:
+            break
 
 total_data_f.close()
 
