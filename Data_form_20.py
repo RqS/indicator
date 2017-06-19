1843e3ba1887922df3ed154b2a1b124a01c042e9
Jiayuan Ding <jiayuanding@guest-126.isi.edu>
2017-06-19 11:49:40 -0700

    add Data_form_20.py

---
+++ Data_form_20.py
@@ -0,0 +1,47 @@
+# coding: utf-8
+import sys
+import random
+
+
+
+total_data_f = open("dataset_20.txt", 'w')
+
+
+for i in range(20):
+    neg_lines = open('incall_neg.txt').read().splitlines()
+    myline =random.choice(neg_lines)
+    total_data_f.write("%s\n" % myline)
+
+
+for i in range(20):
+    neg_lines = open('incall_sn_p.txt').read().splitlines()
+    myline =random.choice(neg_lines)
+    total_data_f.write("%s\n" % myline)
+
+for i in range(20):
+    neg_lines = open('incall_pos.txt').read().splitlines()
+    myline =random.choice(neg_lines)
+    total_data_f.write("%s\n" % myline)
+
+for i in range(20):
+    neg_lines = open('incall_sp_n.txt').read().splitlines()
+    myline =random.choice(neg_lines)
+    total_data_f.write("%s\n" % myline)
+
+for i in range(20):
+    neg_lines = open('incall_p_n.txt').read().splitlines()
+    myline =random.choice(neg_lines)
+    total_data_f.write("%s\n" % myline)
+
+for i in range(20):
+    neg_lines = open('incall_sp_sn.txt').read().splitlines()
+    myline =random.choice(neg_lines)
+    total_data_f.write("%s\n" % myline)
+
+for i in range(20):
+    neg_lines = open('incall_ne.txt').read().splitlines()
+    myline =random.choice(neg_lines)
+    total_data_f.write("%s\n" % myline)
+
+total_data_f.close()
+
