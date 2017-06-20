cf9b0236d2d3624fdd4e319f1e93d1f7af35830d
JiayuanDing100 <jiayuandingjsnu@gmail.com>
2017-06-20 00:09:50 -0700

    modified ft_classifier.py

--- ft_classifier.py
+++ ft_classifier.py
@@ -10,17 +10,59 @@ data = sys.argv[1]
 training_data = sys.argv[2]
 result_data = sys.argv[3]
 true_false = sys.argv[4]
+false_true = sys.argv[5]
 
 data_f = open(data, 'r')   #dataset containing training and test datasets
 train_f = open(training_data, 'w')  # save as trainging datasets
 result_f = codecs.open(result_data, 'w', 'utf-8')  #save as test prediction result
 true_false_f = open(true_false, 'w')
+false_true_f= open(false_true, 'w')
 
 test_text, test_label = pf.generate_train_and_test(data_f, train_f)
 
 data_f.close()
 train_f.close()
 
+classifier = fasttext.supervised(training_data, "incall_experi1_model")
+predict_labels = classifier.predict(test_text)
+TP = 0
+FN = 0
+FP = 0
+TN = 0
+k = 0
+j = 0
+for i in range(len(predict_labels)):
+if test_label[i] == 'TRUE' and predict_labels[i][0].encode('utf-8') == 'TRUE':
+        TP += 1
+elif test_label[i] == 'TRUE' and predict_labels[i][0].encode('utf-8') == 'FALSE':
+        FN += 1
+        k += 1
+        true_false_f.write("%d. %s\n" %(k, test_text[i]) )
+elif test_label[i] == 'FALSE' and predict_labels[i][0].encode('utf-8') == 'TRUE':
+        FP += 1
+        j += 1
+        false_true_f.write("%d. %s\n" %(j, test_text[i]) )
+elif test_label[i] == 'FALSE' and predict_labels[i][0].encode('utf-8') == 'FALSE':
+        TN += 1 
+this_result = {
+        "stats": {
+                "TP": TP,
+                "FN": FN,
+                "FP": FP,
+                "TN": TN,
+                "precision": TP / float(TP + FP),
+                "recall": TP / float(TP + FN)
+                }
+	}
+
+json.dump(this_result, result_f)
+result_f.write("\n")
+
+true_false_f.close()
+false_true_f.close()
+result_f.close()
+
+'''
 #for word_ngrams in [1]:
 num=0
 for epoch in range(4, 21, 1):
@@ -70,4 +112,6 @@ for epoch in range(4, 21, 1):
 			result_f.write("\n")
 true_false_f.close()
 result_f.close()
+'''
+
 
