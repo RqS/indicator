bc7496494e01906c6206bfd856ddf3b8bbffc572
JiayuanDing100 <jiayuandingjsnu@gmail.com>
2017-06-20 09:47:17 -0700

    modified incall_form_dataset

--- incall_form_dataset.py
+++ incall_form_dataset.py
@@ -34,16 +34,12 @@ data_sn_p.close()
 
 total_data_f = open("form_dataset.txt", 'w')
 
-       
-lst = []
+
 for i in range(70000):
     neg_lines = open('/home/ubuntu/dig_indicator/data/incall_data/incall_neg.txt').read().splitlines()
     myline = random.choice(neg_lines)
-    if myline not in lst:
-        dataset.append('__label__FALSE'+' '+myline)
-        lst.append(myline)
-        print i
-
+    dataset.append(('__label__FALSE'+' '+myline).strip() + '\n')
+    print i
 
 random.shuffle(dataset)
 
