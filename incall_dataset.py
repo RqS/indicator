4d0666b2b2f247432cc1d9aede8970aee9d8b9d5
JiayuanDing100 <jiayuandingjsnu@gmail.com>
2017-06-19 21:22:25 -0700

    form a incall dataset

---
+++ incall_dataset.py
@@ -0,0 +1,56 @@
+# coding: utf-8
+import random
+import sys
+
+
+
+
+#data_pos = open("test_140.txt", 'r')
+#data_sp_n = open("incall_sp_sn.txt", 'r')
+#data_sn_p = open("dataset_20.txt", 'r')
+
+
+data_pos = open("incall_pos.txt", 'r')
+data_sp_n = open("incall_sp_n.txt", 'r')
+data_sn_p = open("incall_sn_p.txt", 'r')
+
+
+
+dataset = []
+for line in data_pos:
+    dataset.append('__label__TRUE'+' '+line)
+for line in data_sp_n:
+    dataset.append('__label__TRUE'+' '+line)
+
+print "Negative:",len(dataset)
+
+for line in data_sn_p:
+    dataset.append('__label__FALSE'+' '+line)
+
+data_pos.close()
+data_sp_n.close()
+data_sn_p.close()
+
+
+total_data_f = open("dataset_neg.txt", 'w')
+
+
+lst = []
+k = 0
+for i in range(80000):
+    neg_lines = open('test_140.txt').read().splitlines()
+    myline = random.choice(neg_lines)
+    if myline not in lst:
+        dataset.append('__label__FALSE'+' '+myline)
+        k += 1
+        lst.append(myline)
+        if k ==70000:
+            break
+
+random.shuffle(dataset)
+
+for i in dataset:
+    total_data_f.write("%s" % i)
+
+total_data_f.close()
+
